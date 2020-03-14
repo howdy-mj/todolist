5ddb36fef64cc9ad2d7e2ec85c83b823d01ee751
howdy-mj <hi.minjungkim@gmail.com>
2020-03-14 21:20:44 +0900

    Creating Form Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
-      <TodoListTemplate>
-        App 템플릿 완성
+      <TodoListTemplate form={<Form/>}>
+        템플릿 완성
       </TodoListTemplate>
     );
   }
